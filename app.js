52e6801477eca5c8b6050003a264dd08ec9751f2
socretes <andysharp2003@yahoo.com>
2016-01-13 22:14:27 +0000

    Simple session example

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 var express = require('express');
-var app = express();
-var partials = require('express-partials');
 
+var app = express();
+
+var session = require('express-session');
+var partials = require('express-partials');
+var cookieParser = require('cookie-parser');
+var config = require('./config');
 var errorHandlers = require('./middleware/errorhandlers.js');
 var log = require('./middleware/log.js');
 var routes = require('./routes');
 
 app.set('view engine', 'ejs');
+
+app.use(cookieParser());
 app.use(log.logger);
 app.use(partials());
+app.use(session({
+	secret: config.secret,
+	saveUninitialized: true,
+	resave: true}));
 
 app.set('view options', {defaultLayout: 'layout'});
 
 app.get('/', routes.index);
-app.get('/login', routes.login);
-app.post('/login', routes.loginProcess);
+app.get(config.routes.login, routes.login);
+app.post(config.routes.login, routes.loginProcess);
 app.get('/chat', routes.chat);
 app.use(errorHandlers.notFound);
 app.use(errorHandlers.error);
 
-app.listen(3000);
+app.listen(config.port);
 
 console.log("App server running on port 3000");
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ module.exports.login = login;
 module.exports.loginProcess = loginProcess;
 module.exports.chat = chat;
 function index(req, res){
-  res.render('index', {title: 'Index', message: 'Welcome to Noughts and Crosses'}); 
+  res.cookie('IndexCookie', 'This was set from Index');
+  res.render('index', {title: 'Index', message: 'Welcome to Noughts and Crosses', cookie: JSON.stringify(req.cookies), session: JSON.stringify(req.session)});
 };
+
 function login(req, res){
   res.render('login', {title: 'Login'});
 };
